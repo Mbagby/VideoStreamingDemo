d66cdfb16f595ff17c9fab54a01f3e5370a92aed
Madeline <madelinebagby@yahoo.com>
2016-11-20 11:05:24 -0800

    Fixing assets again

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ Bundler.require(*Rails.groups)
 
 module DemoVideoStreaming
   class Application < Rails::Application
+    config.serve_static_assets = true
 
 
     # Settings in config/environments/* take precedence over those specified here.
